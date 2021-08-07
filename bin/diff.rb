b0c427b09092e035baf06b722247626cebaace11
Tony Bowden <tony@tmtm.com>
2021-08-07 12:24:46 +0300

    Don't report on expected missing members

--- bin/diff.rb
+++ bin/diff.rb
@@ -3,5 +3,12 @@
 
 require 'every_politician_scraper/comparison'
 
+# Not listed on the site
+SKIP = [
+  ['---', 'Ellis Webster', 'Premier of Anguilla']
+].freeze
+
 diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/official.csv').diff
+                                         .reject { |row| SKIP.include? row }
+
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
